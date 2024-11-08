a39c2e4a315f955b80be0db6d25df4342a3ccc3c
WICKY-Arthur <arthur.wicky@edu.univ-fcomte.fr>
2024-11-08 13:56:40 +0100

    Écriture du javascript pour réaliser les intéractions

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 // TODO
+const a_mettre_en_rouge = document.querySelector("#a-mettre-en-rouge");
+a_mettre_en_rouge.style.color = "red";
+
+const en_rouge_suite_a_click = document.querySelector("#en-rouge-suite-a-click")
+en_rouge_suite_a_click.addEventListener("click", (evt) => { en_rouge_suite_a_click.style.color = "red" });
+
+const collH2 = document.querySelectorAll("H2");
+collH2.forEach((elm) => {
+    elm.addEventListener("click", (evt) => {
+        evt.target.style.color = "red"
+    })
+});
